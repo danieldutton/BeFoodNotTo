5108bea1297c8449780c7ddd46b9a8d4d1fc4199
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-04 16:23:49 +0100

    Initial Commit

---
+++ BeFoodNotTo/BeFoodNotToApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFoodNotToApp.swift
+//  BeFoodNotTo
+//
+//  Created by Daniel Dutton on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BeFoodNotToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
