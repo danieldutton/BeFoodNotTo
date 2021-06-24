ddc697904c7774f16f5f7e6221b96419313adcdb
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-24 15:29:07 +0100

    Add CodingKeys enum to 'Establishment' and decoding works
    This is a good base to now work from going forward.

--- BeFoodNotTo/Model/Establishments/Establishment.swift
+++ BeFoodNotTo/Model/Establishments/Establishment.swift
@@ -2,10 +2,19 @@ import Foundation
 
 struct Establishment: Codable {
     let fhrsid: Int
-    let localAuthorityBusinessID: Int
+    let localAuthorityBusinessID: String
     let businessName: String
     let businessType: String? //check this is a strign value as emitted jsonnull
     let ratingValue, ratingDate: String
     //let links: [Link]
-
+    
+    enum CodingKeys: String, CodingKey {
+        case fhrsid = "FHRSID"
+        case localAuthorityBusinessID = "LocalAuthorityBusinessID"
+        case businessName = "BusinessName"
+        case businessType = "BusinessType"
+        case ratingValue = "RatingValue"
+        case ratingDate = "RatingDate"
+        //case links
+    }
 }
