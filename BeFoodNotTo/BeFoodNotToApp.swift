35bbfa4e6b0ae35e4cd3f162f21e5e47e3045805
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-24 14:07:52 +0100

    Add MVVM group structure in main target

--- BeFoodNotTo/BeFoodNotToApp.swift
+++ BeFoodNotTo/BeFoodNotToApp.swift
@@ -1,10 +1,3 @@
-//
-//  BeFoodNotToApp.swift
-//  BeFoodNotTo
-//
-//  Created by Daniel Dutton on 04/06/2021.
-//
-
 import SwiftUI
 
 @main
--- BeFoodNotTo/View/ContentView.swift
+++ BeFoodNotTo/View/ContentView.swift
@@ -1,13 +1,7 @@
-//
-//  ContentView.swift
-//  BeFoodNotTo
-//
-//  Created by Daniel Dutton on 04/06/2021.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
